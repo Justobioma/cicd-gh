9cbb2f89c54f3c1965780af10a1c100820361f6f
Justobioma <justobioma@outlook.com>
2025-07-15 06:27:31 +0100

    Edited Hello world message

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, CI/CD world!");
+        System.out.println("Hello, I am testing CI/CD world with GitHub Actions!");
     }
 }
