1f7c68efa90a76415feb0265076a70d8a7eddca8
Justobioma <justobioma@outlook.com>
2025-07-15 07:28:41 +0100

    I edited the Hello world text to include manual test success

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, I am retesting CI/CD world with GitHub Actions!");
+        System.out.println("Hello, I am able to test manually CI/CD world with GitHub Actions!");
     }
 }
