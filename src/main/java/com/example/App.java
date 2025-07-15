9d198a7e759207e8f2fa73e440dff26c33ad4f9f
Justobioma <justobioma@outlook.com>
2025-07-15 08:29:02 +0100

    Added the 'Run Tests' name to the yaml file

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,13 @@
 package com.example;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, I am able to test manually CI/CD world with GitHub Actions!");
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    void testOutputMessage() {
+        String expected = "Hello, CI/CD world!";
+        assertEquals(expected, App.getGreeting());
     }
 }
